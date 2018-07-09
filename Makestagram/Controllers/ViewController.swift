fca4fbce87a26e08f224714ec0fdddffd41be518
Unknown <giorgos.livas.99@gmail.com>
2018-07-09 11:15:25 -0700

    upload files

---
+++ Makestagram/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by George Livas on 09/07/2018.
+//  Copyright © 2018 George Livas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
