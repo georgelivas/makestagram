e0b52f579564f4b9b4e723bd68f42c879fc566f0
Unknown <giorgos.livas.99@gmail.com>
2018-07-11 10:41:19 -0700

    fix bug code: 11

--- Makestagram/Controllers/CreateUsernameViewController.swift
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -12,6 +12,9 @@ import FirebaseDatabase
 
 class CreateUsernameViewController: UIViewController {
 
+    typealias FIRUser = FirebaseAuth.User
+
+    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     
@@ -27,30 +30,21 @@ class CreateUsernameViewController: UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
-        // 1
         guard let firUser = Auth.auth().currentUser,
             let username = usernameTextField.text,
             !username.isEmpty else { return }
         
-        // 2
-        let userAttrs = ["username": username]
-        
-        // 3
-        let ref = Database.database().reference().child("users").child(firUser.uid)
-        
-        // 4
-        ref.setValue(userAttrs) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
+        UserService.create(firUser, username: username) { (user) in
+            guard let _ = user else {
                 return
             }
             
-            // 5
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                let user = User(snapshot: snapshot)
-                
-                // handle newly created user here
-            })
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            
+            if let initialViewController = storyboard.instantiateInitialViewController() {
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+            }
         }
     }
     /*
